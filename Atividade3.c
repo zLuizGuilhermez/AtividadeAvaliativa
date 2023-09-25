b1695474d218028cfcc29226f655d2f7d2a6ffa7
Luiz Guilherme Oliveira da Mota <137472016+zLuizGuilhermez@users.noreply.github.com>
2023-09-25 14:51:56 -0300

    Update Atividade3.c

--- Atividade3.c
+++ Atividade3.c
@@ -1,37 +1,33 @@
 #include <stdio.h>
 
-main()
-{
-
+int main() {
     float a, b, c;
     int retorno;
 
-    do
-    {
-        printf("Digite o valor de a:");
+    do {
+        printf("Digite o valor de a: ");
         scanf("%f", &a);
-        printf("Digite o valor de b");
+        printf("Digite o valor de b: ");
         scanf("%f", &b);
-        printf("Digite o valor de c");
+        printf("Digite o valor de c: ");
         scanf("%f", &c);
-        if (a > 0 && b > 0 && c > 0){
-            if (a == b && a != c || c == b && c != a || a == c && a != b){
-                printf("e um triangulo isosceles");
+
+        if (a > 0 && b > 0 && c > 0) {
+            if (a == b && b == c) {
+                printf("e um triangulo equilatero\n");
+            } else if (a == b || a == c || b == c) {
+                printf("e um triangulo isosceles\n");
+            } else {
+                printf("e um triangulo escaleno\n");
             }
-            else if(a != b || a != c && b != a || b != c && c != a || c != b ){
-                printf(" e um triangulo escaleno");
-            }
-            else{
-                printf("e um triangulo equilatero");
-            }
-        }
-        else{
-            printf("O valor não pode ser menor que zero.");
+        } else {
+            printf("Os valores nao podem ser menores ou iguais a zero.\n");
         }
 
-        printf("\nDeseja inserir os valores novamente? (1 para sim / 0 para nao)");
+        printf("\nDeseja inserir os valores novamente? (1 para sim / 0 para nao): ");
         scanf("%d", &retorno);
 
     } while (retorno > 0);
-     return 0; 
- }
+
+    return 0;
+}
