a6331c1b7a364b6f72f4d7fa3f59b57e6e798f32
Luiz Guilherme Oliveira da Mota <137472016+zLuizGuilhermez@users.noreply.github.com>
2023-10-03 19:48:54 -0300

    Update Atividade4.c

--- Atividade4.c
+++ Atividade4.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
 #include<math.h>
-int main()
-{
+int main(){
     int a = 0, b = 0, c = 0, calculo = 0, retorno = 0;
     float anguloA = 0, anguloB = 0, anguloC = 0;
-    do
-    {
+    do{
 
         printf("Digite o valor de a:");
         scanf("%d", &a);
@@ -14,8 +12,7 @@ int main()
         printf("Digite o valor de c:");
         scanf("%d", &c);
 
-        if (a + b > c && b + c > a && a + c > b)
-        {
+        if (a + b > c && b + c > a && a + c > b){
             anguloA = acos((b * b + c * c - a * a) / (2 * b * c));
             anguloB = acos((a * a + c * c - b * b) / (2 * a * c));
             anguloC = acos((a * a + b * b - c * c) / (2 * a * b));
@@ -24,21 +21,17 @@ int main()
             anguloB = anguloB * (180.0 / M_PI);
             anguloC = anguloC * (180.0 / M_PI);
 
-            if (anguloA == 90 || anguloB == 90 || anguloC == 90)
-            {
+            if (anguloA == 90 || anguloB == 90 || anguloC == 90){
                 printf("e um triangulo retangulo.");
             }
-            else if (anguloA > 90 || anguloB > 90 || anguloC > 90)
-            {
+            else if (anguloA > 90 || anguloB > 90 || anguloC > 90){
                 printf("e um triangulo obtusangulo.");
             }
-            else
-            {
+            else{
                 printf("e um triangulo acutangulo.");
             }
         }
-        else
-        {
+        else{
             printf("Não é um triângulo válido.");
         }
         printf("\nDeseja adicionar novos valores?");
